e2973814af18c1e87bf2d34150f8e38dd0517101
Long Huynh <longht021189@gmail.com>
2022-07-31 21:09:40 +0700

    check request admin in windows

--- cmd/admin.go
+++ cmd/admin.go
@@ -7,10 +7,11 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
 
 	"github.com/spf13/cobra"
+	"syscall"
+	"golang.org/x/sys/windows"
 )
 
 // adminCmd represents the admin command
@@ -24,6 +25,12 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		if !amAdmin() {
+			runMeElevated()
+		}
+		fmt.Printf("asdasdasd %v", amAdmin());
+
+		/* Linux/OSX,...
 		ex, err := os.Executable()
 		if err != nil {
 			panic(err)
@@ -37,7 +44,7 @@ to quickly create a Cobra application.`,
 			panic(err)
 		}
 
-		fmt.Printf("admin called %v, %v", ex, string(out))
+		fmt.Printf("admin called %v, %v", ex, string(out))*/
 
 		// cmaad := exec.Command("/bin/sh", "-c", "sudo ls")
 		// out, err := cmaad.Output()
@@ -61,4 +68,34 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// adminCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
+func runMeElevated() {
+    verb := "runas"
+    exe, _ := os.Executable()
+    cwd, _ := os.Getwd()
+    args := strings.Join(os.Args[1:], " ")
+
+    verbPtr, _ := syscall.UTF16PtrFromString(verb)
+    exePtr, _ := syscall.UTF16PtrFromString(exe)
+    cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+    argPtr, _ := syscall.UTF16PtrFromString(args)
+
+    var showCmd int32 = 1 //SW_NORMAL
+
+    err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func amAdmin() bool {
+    _, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+    if err != nil {
+        fmt.Println("admin no")
+        return false
+    }
+    fmt.Println("admin yes")
+    return true
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/longht021189/ops-cl
 
 go 1.18
 
-require github.com/spf13/cobra v1.5.0
+require (
+	github.com/spf13/cobra v1.5.0
+	golang.org/x/sys v0.0.0-20220730100132-1609e554cd39
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
